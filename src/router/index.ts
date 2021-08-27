09e59ff1246fda6673dd4bf26dd3848f719ab7b8
fu-zhibin <3447664746@qq.com>
2021-08-27 14:47:25 +0800

    fix: change web  history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 import localCache from "@/utils/cache";
 
@@ -26,7 +26,7 @@ const routes: RouteRecordRaw[] = [
 
 const router = createRouter({
   routes,
-  history: createWebHashHistory()
+  history: createWebHistory()
 });
 router.beforeEach((to, from) => {
   if (to.path !== "/login") {
