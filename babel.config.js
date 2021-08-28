2c68799fae15a40d70c16dcb0943e1ceb53f91b8
fu-zhibin <3447664746@qq.com>
2021-08-28 18:09:38 +0800

    fix: change some config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 const prodPlugins = [];
 // 如果是发布模式则启用的插件
 if (process.env.NODE_ENV === "production") {
-  prodPlugins.push("transform-remove-console");
+  prodPlugins.push("babel-preset-transform-remove-console");
 }
 
 module.exports = {
